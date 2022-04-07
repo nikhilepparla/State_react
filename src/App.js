24ba25ce01cdd9ac0d28b9060696287168943183
Nikhilepparla <e.nikhil1999@gmail.com>
2022-04-07 14:17:12 +0530

    class based state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    person: [
+      { name: 'Nikhil ', age: 19 },
+      { name: 'Ajay ', age: 19 },
+      { name: 'Hrishi ', age: 19 }
+
+    ]
+  }
+  onCliCkEventListener = () => {
+    this.setState ( {
+      person: [
+        { name: 'Amar ', age: 50 },
+        { name: 'Akbar ', age: 40 },
+        { name: 'Antony ', age: 30 }
+      ]
+    })
+   }
+
+  render() {
+    return (
+      <div className="container">
+        <div ><p>My name is {this.state.person[0].name}
+          and i am {this.state.person[0].age} old</p>
+          <p>My name is {this.state.person[1].name}
+          and i am {this.state.person[1].age} old</p>
+          <p>My name is {this.state.person[2].name}
+          and i am {this.state.person[2].age} old</p></div>
+        <button className="btn btn-primary" onClick={this.onCliCkEventListener}>Swap</button>
+      </div>
+    )
+  }
 }
-
 export default App;
